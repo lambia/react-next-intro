43712d2445e8fbfabadee423c73e06df86f5f802
Luca Lambia <luca@lambia.it>
2026-02-17 10:02:12 +0100

    interpolazione variabili e funzioni

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,19 +1,19 @@
 import Head from "next/head";
 import Image from "next/image";
-import { Geist, Geist_Mono } from "next/font/google";
-import styles from "@/styles/Home.module.css";
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+// import styles from "@/styles/Home.module.css";
 
 export default function Home() {
+
+  const numero = 42;
+  const nome = "Luca";
+
+  function raddoppia(numero) {
+    return numero * 2;
+  }
+
+  const x = [1, 22, 333];
+  const y = { nome: "Luca" };
+
   return (
     <>
       <Head>
@@ -22,66 +22,17 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <div
-        className={`${styles.page} ${geistSans.variable} ${geistMono.variable}`}
-      >
-        <main className={styles.main}>
-          <Image
-            className={styles.logo}
-            src="/next.svg"
-            alt="Next.js logo"
-            width={100}
-            height={20}
-            priority
-          />
-          <div className={styles.intro}>
-            <h1>To get started, edit the index.js file.</h1>
-            <p>
-              Looking for a starting point or more instructions? Head over to{" "}
-              <a
-                href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                Templates
-              </a>{" "}
-              or the{" "}
-              <a
-                href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                Learning
-              </a>{" "}
-              center.
-            </p>
-          </div>
-          <div className={styles.ctas}>
-            <a
-              className={styles.primary}
-              href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <Image
-                className={styles.logo}
-                src="/vercel.svg"
-                alt="Vercel logomark"
-                width={16}
-                height={16}
-              />
-              Deploy Now
-            </a>
-            <a
-              className={styles.secondary}
-              href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Documentation
-            </a>
-          </div>
-        </main>
+      <div>
+        <h1>Hello Next World!</h1>
+        <h2>Benvenuto, {nome}</h2>
+        <h3>Il tuo numero è {numero}</h3>
+        <ul>
+          <li>{raddoppia(2)}</li>
+          <li>{raddoppia(4)}</li>
+          <li>{raddoppia(8)}</li>
+          <li>{raddoppia(16)}</li>
+        </ul>
+        <p>{x.join(", ")}</p>
       </div>
     </>
   );
